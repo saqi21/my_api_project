f97a93d412432bc5c14569ad9528b9d7dd28c717
saqi21 <saqibmehar41@gmail.com>
2022-12-29 18:28:27 +0500

    added index view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,10 +1,17 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee
   def index
+    @employee=Employee.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def set_employee
+    @employee=Employee.find(params[:id])
   end
 end
