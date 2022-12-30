4ea749f28837b03c3a65060c2ed8a9d8381652a2
Dev.Salman <sl0429905@gmail.com>
2022-12-30 14:04:37 +0500

    getting response from gkteco_client service

--- app/services/gkteco_clients.rb
+++ app/services/gkteco_clients.rb
@@ -9,23 +9,15 @@ class GktecoClient
         @url = URI("http://localhost:8000/") 
     end
 
-    def fetch_data(key)
+    def fetch(key)
         request = Net::HTTP::Get.new(@url)
         http = Net::HTTP.new(@url.host, @url.port);
-        token = JWT.encode({'admin': 'ammad'},key)
-        request["Authorization"] = "#{token}"
+        token = JWT.encode({'admin': 'devsloop'},key)
+        request["Authorization"] = "Bearer #{token}"
 
         response = http.request(request)
         response.read_body
     end
+end   
 
-end
-
-<<<<<<< HEAD
-        
-        
-
-end
-=======
-GktecoClient.new.fetch_data('devsloop')
->>>>>>> 6d1e87fcdf2df7343767021ac3bad38e379cb1fc
+puts GktecoClient.new.fetch('devsloop')
