e694cde28f9341e41f4871ebc288e1e7ca5948de
Zubair <181370029@gift.edu.pk>
2022-12-30 13:16:08 +0500

    adding gkteco_client

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
+gem 'net-http'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -52,3 +53,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'jwt'
--- app/services/gkteco_clients.rb
+++ app/services/gkteco_clients.rb
@@ -1,8 +1,31 @@
-class GktecoClient
-        def initialize 
-        end
+require "uri"
+require "net/http"
+require 'jwt'
 
+class GktecoClient
+    attr_reader :url
+
+    def initialize
+        @url = URI("http://localhost:8000/") 
+    end
+
+    def fetch_data(key)
+        request = Net::HTTP::Get.new(@url)
+        http = Net::HTTP.new(@url.host, @url.port);
+        token = JWT.encode({'admin': 'ammad'},key)
+        request["Authorization"] = "#{token}"
+
+        response = http.request(request)
+        response.read_body
+    end
+
+end
+
+<<<<<<< HEAD
         
         
 
 end
+=======
+GktecoClient.new.fetch_data('devsloop')
+>>>>>>> 6d1e87fcdf2df7343767021ac3bad38e379cb1fc
