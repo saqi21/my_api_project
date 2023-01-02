0e48de4b4d7b3c6253102496ccda2ba2491928e6
Dev.Salman <sl0429905@gmail.com>
2023-01-02 13:51:48 +0500

    attendance duration

--- app/services/process_attendance.rb
+++ app/services/process_attendance.rb
@@ -19,11 +19,32 @@ class ProcessAttendance
   def get_employees
     employees = []
     self.users.each do |u|
-      employees.append(u) if u['privilege'] == 14
+      employees.append(u) if u['privilege'] == 0
     end
     employees
 	end
 
-  
+  def attendance_for(id)
+    
+  end
+
+  def attendance_during(from, to)
+    current_attendance = []
+    self.attendance_logs.each do |at|
+      selected_time = DateTime.parse(at['timestamp'])
+      puts selected_time >= DateTime.now - 10.days
+      if selected_time >= from and selected_time <= to
+        current_attendance.append(at) 
+      end
+    end
+    current_attendance
+  end
+
+  # def get punctuality_points(from, to)
+  #   points_table = {}
+  #   self.users.each do |user|
+  #     self.attendance_logs.each
+  #   end
+  # end
 
 end
